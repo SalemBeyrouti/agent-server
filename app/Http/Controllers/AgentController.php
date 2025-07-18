cbdb214ee42421c8fe6e1c8ec77d8be06dc09b8a
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-07-18 23:57:04 +0300

    docs: add learning notes and sample code for Eloquent ORM features

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -15,6 +15,28 @@ class AgentController extends Controller
     public function show($id)
     {$agent = Agent::findOrFail($id);
     return new AgentResource($agent);
+
+    public function store($id): RedirectResponse
+
+    //Agent::all retrive all the rechords from the models associatred table
+
+    //Agent::where(you pass conditions) -> you can assign the where conditions ex: orderBy("eamil") ->get() 
+    //Agent::where(...)->first(); u can use fresh()to retrieve the data
+    // or refresh() to refresh the whole database in case something new came up both $flight->refresh or fresh ();
+
+    //chunk is a method in collection soi can use Agent::chunk(10, function (Collection $agents){}) so i retreive subset of modles, first number is the records/chunk, closure passed as the 2nd arg. chunkbyid can be used with where statement
+
+    //cursormethod complicated, check later
+
+    //addSelect advanced filtering, u can insert multiple filtering ::select whereColukmn()-> orderBy() ->limit()
+
+    //::find ::where ::firstWhere directly from model
+
+    //::firstOrCreate at the top if not exist, can be used to rettrive or edit data inside, same as ::firstOrNew
+
+    //
+
+    
 }
 
 }
--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agent extends Model
 {
+
+    //protected $table="my_agents" if i waanna specify the name of the tbale
+
+    //protected $primaryKey = "agent_id" if i wanna also change the name of the id
+
+    //$incrementing = false can be id decrementing or non numeric primary key
+
+    // protected $keyType = "string" swtiching to string 
+
+    //use HasUuids; it means unique alpha numeric identifiers, in addition they can be overide by defining new unique id
+
+    //use HasUlids 26 characters
+
+    // public $timstamps = false if u dont want them in ur db
+
+    //protected $dateFormat = "U"; if u wanna change the format 
+
+
+    //protected $connection = "mysql" if u wanna change the connection for a specific mode
+
+
+    //protected $attributes = ['options' => '[]','delayed' => false,]; if u wanna define the default value
+
+    //preventLazyLoading(! $this->app->isProduction()); i still dont know where this might be useful
+    
     use HasFactory;
 }
